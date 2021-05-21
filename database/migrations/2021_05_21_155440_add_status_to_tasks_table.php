b9cfb9bee383611884b40fed3c54a8cad0810473
chinatsu.haraikawa <pechi72@icloud.com>
2021-05-21 08:50:53 +0000

    migration

--- database/migrations/2021_05_21_155440_add_status_to_tasks_table.php
+++ database/migrations/2021_05_21_155440_add_status_to_tasks_table.php
@@ -27,7 +27,7 @@ class AddStatusToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status', 10);    // statusカラム追加
+            $table->dropColumn('status', 10);    // statusカラム追加
             //
         });
     }
